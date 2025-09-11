9ed3cae116cb5669a715b023e25aa5fcadd59461
Sadik Mohammad <sadiqrangrej10@gmail.com>
2025-09-11 15:45:54 +0530

    Add proper CORS support for frontend

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
-# run.py
-
 from app import create_app
 from flask_cors import CORS
 
 app = create_app()
-CORS(app)  # Enable CORS globally
+
+# Enable CORS and specify allowed origin(s)
+CORS(app, resources={r"/*": {"origins": "https://gn-emitra.netlify.app"}})
 
 if __name__ == "__main__":
     import os
